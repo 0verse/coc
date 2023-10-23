8277bb37cb370a48172c86907835f6ce221d5dec
0verse <ahc@tuta.io>
2023-10-23 19:18:10 +0200

    cocc: main() return type defaults to void

--- cocc.sh
+++ cocc.sh
@@ -4,8 +4,9 @@ coc_file=$1
 c_file=$(basename -s .coc $coc_file).c
 cp $coc_file $c_file
 
-sed -e "1s/^/#include \"coc.h\"\n\n/"    -i $c_file
-sed -e "/^ \+or/ s/\bor\b/else if/g"     -i $c_file
-sed -e "/^ \+esac/ s/\besac\b/default/g" -i $c_file
-sed -e "/^ \+fi/ s/\bfi\b/else/g"        -i $c_file
-sed -e "/^ \+def/ s/\bdef\b/typedef/g"   -i $c_file
+sed -e "1s/^/#include \"coc.h\"\n\n/"          -i $c_file
+sed -e "/^\s\+main/ s/\main\b/void main/g"     -i $c_file
+sed -e "/^\s\+or\(\s\|$\)/ s/\bor\b/else if/g" -i $c_file
+sed -e "/^\s\+fi\(\s\|$\)/ s/\bfi\b/else/g"    -i $c_file
+sed -e "/^\s\+esac/ s/\besac\b/default/g"      -i $c_file
+sed -e "/^\s\+def/ s/\bdef\b/typedef/g"        -i $c_file
