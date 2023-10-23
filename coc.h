192b7b46a95b199a61c08cab9152492fcbc9cf00
0verse <ahc@tuta.io>
2023-10-23 13:50:16 +0200

    cocc: add the CoC C header

---
+++ coc.h
@@ -0,0 +1,66 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+//#ifdef __cplusplus
+//typedef bool b8;
+//#else
+typedef _Bool b8;
+//#endif
+typedef int32_t b32;
+
+typedef float f32;
+typedef double f64;
+
+typedef intptr_t iptr;
+typedef uintptr_t uptr;
+typedef ptrdiff_t isize;
+typedef size_t usize;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+// mostly inspired by:
+// https://nullprogram.com/blog/2023/10/08/
+
+#define sizeof(x)    (isize)sizeof(x)
+#define alignof(x)   (isize)_Alignof(x)
+#define countof(a)   (sizeof(a) / sizeof(*(a)))
+#define lengthof(s)  (countof(s) - 1)
+#define assert(c)  while (!(c)) __builtin_unreachable()
+
+#define s8(s) (s8){(u8 *)s, lengthof(s)}
+typedef struct {
+    u8  *data;
+    isize len;
+} s8;
+
+static s8    s8span(u8 *, u8 *);
+static b32   s8equals(s8, s8);
+static isize s8compare(s8, s8);
+static u64   s8hash(s8);
+static s8    s8trim(s8);
+//static s8    s8clone(s8, arena *);
+
+void print(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+}
+
+void println(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
+    printf("\n");
+}
